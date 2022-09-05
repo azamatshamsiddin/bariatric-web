41ba69d9ebf530cd8349a69a3672fbc51186f111
azamatshamsiddin <muratovichazamat@yandex.com>
2022-09-05 20:13:52 +0500

    home page and about page have done fully responsive

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,5 @@
 // Scroll to top function
-
 const scrollToTopBtn = document.getElementById("scrollToTopBtn");
-
 const goToTop = () => {
 	window.scrollTo({
 		top: 0,
@@ -10,3 +8,14 @@ const goToTop = () => {
 };
 
 scrollToTopBtn.addEventListener("click", goToTop);
+
+const closeBtn = document.querySelector(".nav__close-btn");
+const openBtn = document.querySelector(".header__btn-burger");
+const nav = document.querySelector(".nav");
+
+openBtn.addEventListener("click", () => {
+	nav.classList.remove("nav--close");
+});
+closeBtn.addEventListener("click", () => {
+	nav.classList.add("nav--close");
+});
